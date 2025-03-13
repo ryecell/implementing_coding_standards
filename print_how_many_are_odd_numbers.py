09a869362dd65eec70b9fe4ba12eba4ee59cd330
unknown <janruzzellorquia88@gmail.com>
2025-03-13 19:20:28 +0800

    odd number counter

---
+++ print_how_many_are_odd_numbers.py
@@ -0,0 +1,7 @@
+#initialize value
+number_of_odd = 0
+
+#ask user for 10 numbers
+for num in range(1, 11):
+    number = int(input("Enter 10 numbers. {num}: ".format(num = num)))
+    # check numbers if odd
