7bb859fd76875336ee8037694b9513f3fd21f6db
unknown <janruzzellorquia88@gmail.com>
2025-03-13 16:37:00 +0800

    print_the_bigger_number

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -3,5 +3,11 @@
 num1 = int(input("Enter your first number: "))
 num2 = int(input("Enter your second number: "))
 
-
 # print bigger number
+
+if num1 > num2:
+    print(num1)
+elif num2 > num1:
+    print(num2)
+else:
+    pass
