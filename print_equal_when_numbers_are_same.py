bacacfb300555235957c5959a1c4263d59df53e6
unknown <janruzzellorquia88@gmail.com>
2025-03-13 16:43:28 +0800

    print_equal_when_numbers_are_same

---
+++ print_equal_when_numbers_are_same.py
@@ -0,0 +1,9 @@
+# ask user for 2 numbers
+num1 = int(input("Enter your 1st number: "))
+num2 = int(input("Enter your 2nd number: "))
+
+# print "Equal if numbers are same"
+if num1 == num2:
+    print("Equal")
+else:
+    pass
