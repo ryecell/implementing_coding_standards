0735884fcccd9314a98e3c11680bb5d1759f9fc6
unknown <janruzzellorquia88@gmail.com>
2025-03-13 19:14:58 +0800

    print_the_sum_of_10_numbers

--- print_the_sum_of_10_numbers.py
+++ print_the_sum_of_10_numbers.py
@@ -1,4 +1,8 @@
-
 # ask the user to input 10 numbers
+sum_numbers = 0
 for num in range(1, 11):
     number = int(input("Enter 10 numbers. {num}: ".format(num = num)))
+    sum_numbers += number
+
+#print total
+print(sum_numbers)
