7318b43e351192c421d796d4b3117610595e3438
unknown <janruzzellorquia88@gmail.com>
2025-03-13 19:22:32 +0800

    odd numbers counter

--- print_how_many_are_odd_numbers.py
+++ print_how_many_are_odd_numbers.py
@@ -5,3 +5,8 @@ number_of_odd = 0
 for num in range(1, 11):
     number = int(input("Enter 10 numbers. {num}: ".format(num = num)))
     # check numbers if odd
+    if number % 2 == 1:
+        number_of_odd += 1
+        
+#print number of odd
+print(number_of_odd)    
