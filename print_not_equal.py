026016eef626faf167e371e0f16e528b02a7a6c4
unknown <janruzzellorquia88@gmail.com>
2025-03-13 21:24:26 +0800

    not equal checker

--- print_not_equal.py
+++ print_not_equal.py
@@ -2,3 +2,9 @@
 num1 = int(input("Enter your 1st number: "))
 num2 = int(input("Enter your 2nd number: "))
 
+# check if two numbers are equal
+if num1 != num2:
+    #print not equal
+    print("Not Equal")
+else:
+    pass
