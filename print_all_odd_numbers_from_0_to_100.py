89352a799b7e5dffa380e148fa13464bd2bd211d
unknown <janruzzellorquia88@gmail.com>
2025-03-13 23:25:20 +0800

    all odd numbers from 0 to 100

---
+++ print_all_odd_numbers_from_0_to_100.py
@@ -0,0 +1,10 @@
+#initialize value
+starting_number = 0
+
+# add 1 to starting_number until it equals to 0 while scanning for odd numbers
+while starting_number < 100:
+    #print if odd number
+    if starting_number % 2 != 0:
+        print(starting_number)
+    starting_number += 1
+
