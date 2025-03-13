1bc37f06fb2dc2b00eada2e3bf2116b29d2fd462
unknown <janruzzellorquia88@gmail.com>
2025-03-13 19:30:21 +0800

    all numbers except multiples of 10

---
+++ print_numbers_from_0_to_100_except_ending_in_0.py
@@ -0,0 +1,5 @@
+for num in range(100): #100 is used as it ends with zero so it won't be included in the output
+    #check if num is not divisible by 10
+    if num % 10 != 0:
+        #print numbers
+        print(num)
