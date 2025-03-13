800f25c11f5b8a3faf1d5f4d361c0888329cc1a8
unknown <janruzzellorquia88@gmail.com>
2025-03-13 22:28:53 +0800

    quotient without decimals

--- quotient_without_decimal_point.py
+++ quotient_without_decimal_point.py
@@ -1,3 +1,7 @@
 #ask user to input 2 numbers
 num1 = int(input("Enter your 1st number: "))
 num2 = int(input("Enter your 2nd number: "))
+
+#print quotient
+quotient = num1/num2 
+print(f"{quotient:.0f}")
