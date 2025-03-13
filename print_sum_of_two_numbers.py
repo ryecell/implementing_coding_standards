f6c444eedecce46ca142479aaae144f52fcf200b
unknown <janruzzellorquia88@gmail.com>
2025-03-13 16:50:50 +0800

    print_sum_of_two_numbers

--- print_sum_of_two_numbers.py
+++ print_sum_of_two_numbers.py
@@ -1,4 +1,6 @@
-# # ask user for 2 numbers
+# ask user for 2 numbers
 num1 = int(input("Enter your 1st number: "))
 num2 = int(input("Enter your 2nd number: "))
 
+# print the sum 
+print(num1 + num2)
