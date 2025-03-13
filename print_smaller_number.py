7145eec2503e96e483cccbf66164723845f06963
unknown <janruzzellorquia88@gmail.com>
2025-03-13 21:11:47 +0800

    smaller number printer

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,3 +1,11 @@
 # ask user for 2 numbers
 num1 = int(input("Enter your 1st number: "))
 num2 = int(input("Enter your 2nd number: "))
+
+#check smaller number
+if num1 < num2:
+    print(num1)
+elif num2 < num1:
+    print(num2)
+else:
+    pass
