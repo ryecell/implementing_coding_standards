7e5b8b73f404468f858348a23ea7e2ec9411ed60
unknown <janruzzellorquia88@gmail.com>
2025-03-14 10:43:17 +0800

    update

--- display_numbers_without_duplicates.py
+++ display_numbers_without_duplicates.py
@@ -1,9 +1,11 @@
 user_list = []
+num_list = []
 #ask the user for input
 for num in range(10):
-    num_input = int(input("Enter num{num}: ").format(num = num))
+    num_input = int(input("Enter num{num}: ".format(num = num+1)))
     user_list.append(num_input)
 
-if num_input not in user_list:
-    print(user_list)
-    
+for num_input in user_list:
+    if num_input not in num_list:
+        num_list.append(num_input)
+print(num_list)
