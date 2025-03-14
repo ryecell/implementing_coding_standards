a9c49bd33af9d6ca59fab555b5e03d7b19e915d9
unknown <janruzzellorquia88@gmail.com>
2025-03-14 15:38:04 +0800

    initial counter failed

--- display_number_with_most_duplicates.py
+++ display_number_with_most_duplicates.py
@@ -1,9 +1,21 @@
 #using while loop to ask user for inputs until input is invalid
 user_list = []
+counter_list = []
 while True:
     try:
         user_input = int(input("Enter your number: "))
         user_list.append(user_input)
-    except ValueError:
+
+    except ValueError: #filter if user input is invalid
         print("Input invalid. Stopping program...")
         break
+
+#count frequencies    
+for num in user_list:
+    count = user_list.count(num)
+    counter_list.append(count)
+    
+# initial counter
+print(str(max(counter_list)))
+
+    
