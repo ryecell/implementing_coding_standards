d25b537cb7dac4004923958e22e0ca589a4fc755
unknown <janruzzellorquia88@gmail.com>
2025-03-14 12:06:21 +0800

    updated arrangement

--- display_first_occurance_of_numbers.py
+++ display_first_occurance_of_numbers.py
@@ -1,11 +1,12 @@
 # intialize list and variables
 user_list = []
-list_set = set(user_list) # save the list into a set to remove duplicates and save first entry
 
 # ask user for 10 inputs
 for numbers in range(10):
     user_input = (int(input("Enter 10 numbers. Num {numbers}: ".format(numbers = numbers + 1))))
     user_list.append(user_input)
+    
+list_set = set(user_list) # save the list into a set to remove duplicates and save first entry
 
 #print set
 print(list_set)
