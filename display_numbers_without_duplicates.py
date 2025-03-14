fd0126d6bd39df4d6ed4c2cddc4565adf0783bcc
unknown <janruzzellorquia88@gmail.com>
2025-03-14 11:07:26 +0800

    update2

--- display_numbers_without_duplicates.py
+++ display_numbers_without_duplicates.py
@@ -1,11 +1,18 @@
 user_list = []
 num_list = []
+unique_list = []
 #ask the user for input
 for num in range(10):
     num_input = int(input("Enter num{num}: ".format(num = num+1)))
     user_list.append(num_input)
 
-for num_input in user_list:
-    if num_input not in num_list:
-        num_list.append(num_input)
+#filtering duplicates
+for unique_numbers in user_list:
+    if unique_numbers not in num_list:
+        num_list.append(unique_numbers)
+
+
+
 print(num_list)
+
+  
