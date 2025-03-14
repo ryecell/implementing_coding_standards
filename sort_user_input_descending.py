3a648e2974e7967d77179b54c5f9d0941a8d1c67
unknown <janruzzellorquia88@gmail.com>
2025-03-14 16:03:13 +0800

    first trial

---
+++ sort_user_input_descending.py
@@ -0,0 +1,12 @@
+#initialize list variable
+user_list = []
+
+#using while loop to ask user for inputs until input is invalid
+while True:
+    try:
+        user_input = int(input("Enter your number: "))
+        user_list.append(user_input)
+
+    except ValueError: #filter if user input is invalid
+        print("Input invalid. Stopping program...")
+        break
